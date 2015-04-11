8360795cb862a956ed5eb58ec03231a9cdc14625
Marko Bošković <bosskovic@gmail.com>
2015-04-12 00:04:53 +0200

    updates localized tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,29 +61,54 @@ module ApplicationHelper
       content_tag(:div, class: 'localized-tab tab-content') { raw content_tabs.join }
   end
 
-  def localized_tabs_preview(page, field_name, content_partial, options={})
+
+  def localized_tabs_preview(page, content_partial, options={})
     navs_tabs = []
     content_tabs = []
-    Language.all.each_with_index do |language, index|
-      localized_page = page.localized_pages.find_by_language_id(language.id)
 
-      name = "#{field_name}_#{language.name.underscore}_#{page.id}"
+    page.localized_pages.each_with_index do |localized_page, index|
+      language = localized_page.language
 
       nav = content_tag :li, role: 'presentation', class: (index == 0 ? 'active' : '') do
         caption = "<i class='flag-icon flag-icon-#{language.flag}'></i> #{language.name}"
-        link_to caption.html_safe, "##{name.parameterize}", 'aria-controls' => name, role: 'tab', data: { toggle: 'tab' }
+        link_to caption.html_safe, "##{localized_page.slug}", 'aria-controls' => localized_page.slug, role: 'tab', data: { toggle: 'tab' }
       end
       navs_tabs << nav
 
-      content_tab = content_tag :div, id: "#{name.parameterize}", role: 'tabpanel', class: (index == 0 ? 'tab-pane active' : 'tab-pane') do
-        content = localized_page.send(field_name)
-        render partial: content_partial, locals: { localized_string: content, code: language.locale, content: page }
+      content = content_tag :div, id: "#{localized_page.slug}", role: 'tabpanel', class: (index == 0 ? 'tab-pane active' : 'tab-pane') do
+        render partial: content_partial, locals: { page: page, localized_page: localized_page }
       end
-      content_tabs << content_tab
+      content_tabs << content
     end
 
-    content_tag(:ul, class: 'nav nav-tabs', role: 'tablist') { raw navs_tabs.join } +
-      content_tag(:div, class: 'tab-content') { raw content_tabs.join }
+    content_tag(:ul, class: 'localized-nav-tabs nav nav-tabs', role: 'tablist') { raw navs_tabs.join } +
+      content_tag(:div, class: 'localized-tab tab-content') { raw content_tabs.join }
   end
+
+
+  # def localized_tabs_preview(page, field_name, content_partial, options={})
+  #   navs_tabs = []
+  #   content_tabs = []
+  #   Language.all.each_with_index do |language, index|
+  #     localized_page = page.localized_pages.find_by_language_id(language.id)
+  #
+  #     name = "#{field_name}_#{language.name.underscore}_#{page.id}"
+  #
+  #     nav = content_tag :li, role: 'presentation', class: (index == 0 ? 'active' : '') do
+  #       caption = "<i class='flag-icon flag-icon-#{language.flag}'></i> #{language.name}"
+  #       link_to caption.html_safe, "##{name.parameterize}", 'aria-controls' => name, role: 'tab', data: { toggle: 'tab' }
+  #     end
+  #     navs_tabs << nav
+  #
+  #     content_tab = content_tag :div, id: "#{name.parameterize}", role: 'tabpanel', class: (index == 0 ? 'tab-pane active' : 'tab-pane') do
+  #       content = localized_page.send(field_name)
+  #       render partial: content_partial, locals: { localized_string: content, code: language.locale, content: page }
+  #     end
+  #     content_tabs << content_tab
+  #   end
+  #
+  #   content_tag(:ul, class: 'nav nav-tabs', role: 'tablist') { raw navs_tabs.join } +
+  #     content_tag(:div, class: 'tab-content') { raw content_tabs.join }
+  # end
 
 end

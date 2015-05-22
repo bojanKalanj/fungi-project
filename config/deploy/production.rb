29d309d34963ed7cd8f13af02cec6808e6745d98
Marko Bošković <bosskovic@gmail.com>
2015-05-22 23:11:38 +0200

    adds deployment setup

---
+++ config/deploy/production.rb
@@ -0,0 +1,8 @@
+set :stage, :production
+set :branch, 'master'
+
+set :deploy_to, '/usr/share/nginx/fungiorbis_rails'
+set :rails_env, :production
+set :passenger_restart_options, -> { "#{deploy_to}/current --ignore-app-not-running" }
+
+server 'fungiorbis', user: 'markob', roles: %w{db web app}, primary: true

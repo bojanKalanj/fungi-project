b11c53e7054a6689e4567a3683dd651e01fec989
Marko Bošković <bosskovic@gmail.com>
2015-05-24 03:00:04 +0200

    adds airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,3 @@
-# Airbrake.configure do |config|
-#   config.api_key = '6afdc547b8041cf7ffd3f76584d285e1'
-# end
+Airbrake.configure do |config|
+  config.api_key = '6afdc547b8041cf7ffd3f76584d285e1'
+end

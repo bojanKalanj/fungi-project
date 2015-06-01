9d81daa77ee47d8f586bd79de27e59072e169275
Marko Bošković <bosskovic@gmail.com>
2015-06-01 10:37:29 +0200

    adds all systematics categories to typeahead; fixes the styles and dynamically adjusts menu dimensions

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -15,37 +15,41 @@ $(document).on('ready page:load', function () {
     $form.find('input.systematics').attr('placeholder', $(this).data('placeholder'))
   });
 
-  var bhGenus = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.whitespace,
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: "/systematics/genus.json"
-  });
+  var bloodhound = {};
+  bloodhound.genus = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/genus.json" });
+  bloodhound.familia = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/familia.json" });
+  bloodhound.ordo = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/ordo.json" });
+  bloodhound.subclassis = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/subclassis.json" });
+  bloodhound.classis = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/classis.json" });
+  bloodhound.subphylum = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/subphylum.json" });
+  bloodhound.phylum = new Bloodhound({ datumTokenizer: Bloodhound.tokenizers.whitespace, queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: "/systematics/phylum.json" });
 
 
-  var bhFamilia = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.whitespace,
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: "/systematics/familia.json"
-  });
+  var $systematicsTypeahead = $('.systematics.typeahead');
 
-  $('.systematics.typeahead').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1
-  }, {
-    name: 'genus',
-    source: bhGenus
-//    ,
-//    templates: {
-//      header: '<h3 class="league-name">NBA Teams</h3>'
-//    }
-  }, {
-    name: 'familia',
-    source: bhFamilia
-//      ,
-//      templates: {
-//        header: '<h3 class="league-name">NHL Teams</h3>'
-//      }
-  });
+  $systematicsTypeahead.typeahead({ hint: true, highlight: true, minLength: 1 },
+    systematicsTypeaheadSettings('genus'),
+    systematicsTypeaheadSettings('familia'),
+    systematicsTypeaheadSettings('ordo'),
+    systematicsTypeaheadSettings('subclassis'),
+    systematicsTypeaheadSettings('classis'),
+    systematicsTypeaheadSettings('subphylum'),
+    systematicsTypeaheadSettings('phylum')
+  );
 
+  function systematicsTypeaheadSettings(name) {
+    return { name: name, source: bloodhound[name],
+      templates: { header: '<h3 class="systematics-category">' + $systematicsTypeahead.data(name) + '</h3>' }
+    }
+  }
+  var $systematicsTypeaheadMenu = $('.sidebar-search .tt-menu');
+  $systematicsTypeaheadMenu.css('max-height', $(window).height() - $systematicsTypeahead.offset().top - 1);
+  $systematicsTypeaheadMenu.css('width', $systematicsTypeahead.width());
 });

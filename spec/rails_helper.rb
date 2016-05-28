7c412563d8a3b4752abb8bfe8982ddddc37129b4
Marko Bošković <bosskovic@gmail.com>
2016-05-28 17:41:30 +0200

    adds simplecov; removes manual rspec call from shippable

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+require 'simplecov-csv'
+SimpleCov.formatter = SimpleCov::Formatter::CSVFormatter
+SimpleCov.coverage_dir(ENV["COVERAGE_REPORTS"])
+SimpleCov.start 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
